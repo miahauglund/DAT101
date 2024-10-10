1d5dd6762215ffb45b6c6f7200ca9f8541c04c17
miahauglund <mchauglund@uia.no>
2024-10-10 14:57:32 +0200

    Task 3.1 part 1.2.3

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -5,12 +5,39 @@ initPrintOut(document.getElementById("txtOut"));
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
-}
-printOut(newLine);
+/*let wakeUpTime = 6 ;
+//if (wakeUpTime === 8 ) {
+  printOut("I can catch the bus to school.");
 
+} else {
+ printOut(" I cannot catch the bus to school.");
+} 
+
+let wakeUpTime1 = 7 ;
+if (wakeUpTime1 === 8 ) {
+  printOut("I can catch the bus to school.");
+
+} else {
+ printOut(" I cannot catch the bus to school.");
+} */
+
+let wakeUpTime2 = 8 ;
+if (wakeUpTime2 === 8 ) {
+  printOut("I can catch the bus to school.");
+
+} else {
+ printOut(" I cannot catch the bus to school.");
+} 
+let wakeUpTime3 = 8;
+if (wakeUpTime3 === 7){
+  printOut("I can take the bus to school.")
+
+} else if (wakeUpTime3 === 8){
+  printOut(" I can take the train to school.")
+
+}else {
+  printOut(" I have to take the car to school")
+} 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");

14cf219eb301e2afdcf9080f4195581e7e9f6c97
miahauglund <mchauglund@uia.no>
2024-10-24 14:27:12 +0200

    Task 3.2 part 7

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -49,7 +49,51 @@ printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut (" Dice Rolling extravaganza");
+
+const d1 = Math.ceil(Math.random() * 6);
+const d2 = Math.ceil(Math.random() * 6);
+const d3 = Math.ceil(Math.random() * 6);
+const d4 = Math.ceil(Math.random() * 6);
+const d5 = Math.ceil(Math.random() * 6);
+const d6 = Math.ceil(Math.random() * 6);
+
+let diceThrow = "";
+diceThrow += d1.toString()+ ",";
+diceThrow += d2.toString()+ ",";
+diceThrow += d3.toString()+ ",";
+diceThrow += d4.toString()+ ",";
+diceThrow += d5.toString()+ ",";
+diceThrow += d6.toString();
+
+printOut ("diceThrow : "+ diceThrow);
+
+const count1 = (diceThrow.match(/1/g) || "").length;
+const count2 = (diceThrow.match(/2/g) || "").length;
+const count3 = (diceThrow.match(/3/g) || "").length;
+const count4 = (diceThrow.match(/4/g) || "").length;
+const count5 = (diceThrow.match(/5/g) || "").length;
+const count6 = (diceThrow.match(/6/g) || "").length;
+
+let diceCount = "";
+diceCount += count1.toString() + ",";
+diceCount += count2.toString() + ",";
+diceCount += count3.toString() + ",";
+diceCount += count4.toString() + ",";
+diceCount += count5.tostring() + ",";
+diceCount += count6.toString();
+printOut ("Dicecount : " + diceCount)
+
+const equals1 = (diceCount.match (/1/g) || "").length;
+const equals6 = (diceCount.match(/6/g) || "").length;
+printOut ("Equals1: " + equals1.toString());
+printOut ("Equals6 : " + equals6.toString ());
+
+if (equals1 === 6){
+    printOut ("Full straight");
+} else if (equals6 === 1 ){
+    printOut("Yatzy!!");
+}
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
